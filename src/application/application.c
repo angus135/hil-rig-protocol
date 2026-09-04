50e701d95a11decef413c71bb9417c3bcc7188fb
tvog0001 <166362198+tvog0001@users.noreply.github.com>
2026-09-04 17:03:05 +1000

    DEV-151
    Clang format

--- src/application/application.c
+++ src/application/application.c
@@ -584,7 +584,8 @@ HIL_APPLICATION_Decode_Message( const HIL_Application_Context_T* context,
     {
         return HIL_APPLICATION_STATUS_INVALID_ARGUMENT;
     }
-    if ( max_encoded_message_size < HIL_APPLICATION_HEADER_SIZE_BYTES + 1 )  // +1 for the payload end flag
+    if ( max_encoded_message_size
+         < HIL_APPLICATION_HEADER_SIZE_BYTES + 1 )  // +1 for the payload end flag
     {
         return HIL_APPLICATION_STATUS_BUFFER_TOO_SMALL;
     }
